e73731f2c8b9a8e2f090673de0558f2a69634612
rizalazky <rizalazky26@gmail.com>
2024-09-02 09:22:16 +0700

    update permission seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,56 +25,84 @@ class DatabaseSeeder extends Seeder
 
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
-        
-        Permission::create(['name' => 'lihat user']);
-        Permission::create(['name' => 'hapus user']);
-        Permission::create(['name' => 'update user']);
-        Permission::create(['name' => 'buat user']);
-
-        Permission::create(['name' => 'lihat role']);
-        Permission::create(['name' => 'hapus role']);
-        Permission::create(['name' => 'update role']);
-        Permission::create(['name' => 'buat role']);
-
-        Permission::create(['name' => 'lihat laporan']);
-
-        Permission::create(['name' => 'lihat akses']);
-
+        // Permission::create(['name' => 'manage kategori produk']);
         Permission::create(['name' => 'lihat kategori produk']);
-        Permission::create(['name' => 'hapus kategori produk']);
+        Permission::create(['name' => 'tambah kategori produk']);
         Permission::create(['name' => 'update kategori produk']);
-        Permission::create(['name' => 'buat kategori produk']);
+        Permission::create(['name' => 'hapus kategori produk']);
 
+        // Permission::create(['name' => 'manage unit produk']);
         Permission::create(['name' => 'lihat unit produk']);
         Permission::create(['name' => 'hapus unit produk']);
         Permission::create(['name' => 'update unit produk']);
-        Permission::create(['name' => 'buat unit produk']);
+        Permission::create(['name' => 'tambah unit produk']);
 
+        // Permission::create(['name' => 'manage produk']);
         Permission::create(['name' => 'lihat produk']);
         Permission::create(['name' => 'hapus produk']);
         Permission::create(['name' => 'update produk']);
-        Permission::create(['name' => 'buat produk']);
+        Permission::create(['name' => 'tambah produk']);
 
-
+        // Permission::create(['name' => 'manage customer']);
         Permission::create(['name' => 'lihat customer']);
         Permission::create(['name' => 'hapus customer']);
         Permission::create(['name' => 'update customer']);
-        Permission::create(['name' => 'buat customer']);
-
+        Permission::create(['name' => 'tambah customer']);
+        
+        // Permission::create(['name' => 'manage supplier']);
         Permission::create(['name' => 'lihat supplier']);
         Permission::create(['name' => 'hapus supplier']);
         Permission::create(['name' => 'update supplier']);
-        Permission::create(['name' => 'buat supplier']);
+        Permission::create(['name' => 'tambah supplier']);
 
+        // Permission::create(['name' => 'manage transaksi']);
+
+        // Permission::create(['name' => 'manage transaksi masuk']);
         Permission::create(['name' => 'lihat transaksi masuk']);
         Permission::create(['name' => 'hapus transaksi masuk']);
         Permission::create(['name' => 'update transaksi masuk']);
-        Permission::create(['name' => 'buat transaksi masuk']);
+        Permission::create(['name' => 'tambah transaksi masuk']);
 
+        // Permission::create(['name' => 'manage transaksi keluar']);
         Permission::create(['name' => 'lihat transaksi keluar']);
         Permission::create(['name' => 'hapus transaksi keluar']);
         Permission::create(['name' => 'update transaksi keluar']);
-        Permission::create(['name' => 'buat transaksi keluar']);
+        Permission::create(['name' => 'tambah transaksi keluar']);
+
+
+        // Permission::create(['name' => 'manage stok']);
+
+        // Permission::create(['name' => 'manage stok masuk']);
+        Permission::create(['name' => 'lihat stok masuk']);
+        Permission::create(['name' => 'hapus stok masuk']);
+        Permission::create(['name' => 'update stok masuk']);
+        Permission::create(['name' => 'tambah stok masuk']);
+
+        // Permission::create(['name' => 'manage stok keluar']);
+        Permission::create(['name' => 'lihat stok keluar']);
+        Permission::create(['name' => 'hapus stok keluar']);
+        Permission::create(['name' => 'update stok keluar']);
+        Permission::create(['name' => 'tambah stok keluar']);
+        
+        // Permission::create(['name' => 'manage laporan']);
+        Permission::create(['name' => 'lihat laporan']);
+        
+        // Permission::create(['name' => 'manage user']);
+        Permission::create(['name' => 'lihat user']);
+        Permission::create(['name' => 'hapus user']);
+        Permission::create(['name' => 'update user']);
+        Permission::create(['name' => 'tambah user']);
+
+        // Permission::create(['name' => 'manage role']);
+        Permission::create(['name' => 'lihat role']);
+        Permission::create(['name' => 'hapus role']);
+        Permission::create(['name' => 'update role']);
+        Permission::create(['name' => 'tambah role']);
+
+       
+
+        // Permission::create(['name' => 'manage akses']);
+        Permission::create(['name' => 'lihat akses']);
 
         $role = Role::create(['name' => 'Super-Admin']);
 
